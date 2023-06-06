87db53209882ff99b5bbf8c3003bedd73933aadd
Drysiek <505rysiek@gmail.com>
2023-06-06 15:35:50 +0200

    list4- scatterplot added

--- List4- Machine Learning/NaiveBayesClassifier.py
+++ List4- Machine Learning/NaiveBayesClassifier.py
@@ -12,26 +12,73 @@ def calculate_prior(df):
 def calculate_likelihood_gaussian(df, feat_name, feat_val, label):
     df = df[df[10] == label]
     mean, std = df[feat_name].mean(), df[feat_name].std()
+    if std == 0:
+        return 0.0
     p_x_given_y = (1 / (np.sqrt(2 * np.pi) * std)) * np.exp(-((feat_val-mean)**2 / (2 * std**2)))
+
     return p_x_given_y
+
+
+def calculate_likelihood_categorical(df, feat_name, feat_val, label):
+    df = df[df[10] == label]
+    p_x_given_y = len(df[df[feat_name] == feat_val]) / len(df)
+    return p_x_given_y
+
+
+def get_max(post_prob, labels):
+    maximum = post_prob[0]
+    max_label = labels[0]
+    for i in range(1, len(post_prob)):
+        if post_prob[i] > maximum:
+            maximum = post_prob[i]
+            max_label = labels[i]
+    return max_label
 
 
 def naive_bayes_gaussian(df, x_test):
     features = list(range(1, 10))
     prior = calculate_prior(df)
+    labels = sorted(list(df[10].unique()))
 
     y_predicted = []
     for index, x in x_test.iterrows():
-        labels = [x for x in range(1, 8) if x != 4]
-        likelihood = [1]*len(labels)
+        likelihood = [1.0]*len(labels)
         for j in range(len(labels)):
             for i in range(len(features)):
-                likelihood[j] *= calculate_likelihood_gaussian(df, features[i], x[i], labels[j])
+                likelihood[j] += calculate_likelihood_gaussian(df, features[i], x[i], labels[j])
 
         post_prob = [1]*len(labels)
         for j in range(len(labels)):
             post_prob[j] = likelihood[j] * prior[j]
 
-        y_predicted.append(np.argmax(post_prob))
+        # y_predicted.append(get_max(post_prob, labels))
+        y_predicted.append(np.argmin(post_prob))
+
+    return np.array(y_predicted)
+
+
+def naive_bayes_categorical(df, x_test):
+    # get feature names
+    features = list(df.columns)[:-1]
+
+    # calculate prior
+    prior = calculate_prior(df)
+
+    y_predicted = []
+    # loop over every data sample
+    for index, x in x_test.iterrows():
+        # calculate likelihood
+        labels = sorted(list(df[10].unique()))
+        likelihood = [0]*len(labels)
+        for j in range(len(labels)):
+            for i in range(len(features)):
+                likelihood[j] += calculate_likelihood_categorical(df, features[i], x[i], labels[j])
+
+        # calculate posterior probability (numerator only)
+        post_prob = [1]*len(labels)
+        for j in range(len(labels)):
+            post_prob[j] = likelihood[j] * prior[j]
+
+        y_predicted.append(get_max(post_prob, labels))
 
     return np.array(y_predicted)
--- List4- Machine Learning/main.py
+++ List4- Machine Learning/main.py
@@ -2,10 +2,12 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import StandardScaler
-from NaiveBayesClassifier import naive_bayes_gaussian
+from NaiveBayesClassifier import naive_bayes_gaussian, naive_bayes_categorical
+from matplotlib import pyplot as plt
 
 
 test_size = .2
+reach = range(0, int(214 * test_size))
 
 
 def get_data():
@@ -36,16 +38,23 @@ def get_stats(data_frame):
         print("Min value", data_frame[i].unique().min())
 
 
-def get_cross_validation(data_frame, answers):
-    x_train, x_test, y_train, y_test = train_test_split(data_frame, answers, test_size=.20, random_state=40)
+def get_correlation_heatmap(data_frame):
+    import seaborn as sns
+    import matplotlib.pyplot as plt
 
-    # cv_temp_glass_type = data_frame.groupby(df[10])
-    # cv_glass_type = []
-    # for i in temp_glass_type:
-    #     glass_type.append(i[1])
-    # pass
+    corr = data_frame.iloc[:, 1:-1].corr(method="pearson")
+    cmap = sns.diverging_palette(250, 354, 80, 60, center='dark', as_cmap=True)
 
-    return x_train, x_test, y_train, y_test
+    plt.figure(figsize=(10, 8))  # Set the size of the figure
+    sns.heatmap(corr, vmax=1, vmin=-.5, cmap=cmap, square=True, linewidths=.2)
+
+    plt.title("Correlation Heatmap")  # Set the title of the plot
+    plt.show()
+
+
+def push_4_to_data(data_frame):
+    data_frame[10] = data_frame[10].replace({7: 4})
+    return data_frame
 
 
 def discretization(data_frame, precision=10, starting_position=4):
@@ -54,22 +63,36 @@ def discretization(data_frame, precision=10, starting_position=4):
         _, bins = pd.qcut(data_frame[i], precision=2, q=precision, retbins=True, labels=False, duplicates='drop')
         labels = list(range(starting_position, starting_position + len(bins) - 1))
         discretized_data[i] = pd.cut(data_frame[i], bins=bins, labels=labels, include_lowest=True)
+        discretized_data = discretized_data.astype(float)
     return discretized_data
 
 
 def normalization(data_frame):
     normalized_data = data_frame.copy()
     scaler = MinMaxScaler()
-    normalized_data = scaler.fit_transform(normalized_data)
-    normalized_data = pd.DataFrame(normalized_data)
+
+    first_column = normalized_data.iloc[:, 0]
+    last_column = normalized_data.iloc[:, -1]
+
+    normalized_data.iloc[:, 1:-1] = scaler.fit_transform(normalized_data.iloc[:, 1:-1])
+
+    normalized_data.iloc[:, 0] = first_column
+    normalized_data.iloc[:, -1] = last_column
+
     return normalized_data
 
 
 def standardization(data_frame):
     standardized_data = data_frame.copy()
     scaler = StandardScaler()
-    standardized_data = scaler.fit_transform(standardized_data)
-    standardized_data = pd.DataFrame(standardized_data)
+
+    first_column = standardized_data.iloc[:, 0]
+    last_column = standardized_data.iloc[:, -1]
+
+    standardized_data.iloc[:, 1:-1] = scaler.fit_transform(standardized_data.iloc[:, 1:-1])
+
+    standardized_data.iloc[:, 0] = first_column
+    standardized_data.iloc[:, -1] = last_column
     return standardized_data
 
 
@@ -79,40 +102,83 @@ def compare(y_predicted, y_true):
         # print(i, j)
         if j == i:
             correctly_predicted += 1
-    return correctly_predicted
+    return correctly_predicted, len(y_true)
 
 
-def show_bayes(data, text):
-    train, test = train_test_split(data, test_size=test_size, random_state=40)
-    # x_train = train.drop([10], axis=1)
-    # y_train = train[10]
-    x_test = test.drop([10], axis=1)
-    y_test = test[10]
+def show_naive_bayes(train, x_test, y_test, text, color):
     y_predicted = naive_bayes_gaussian(train, x_test)
-    print(text, ':\t', compare(y_predicted, y_test))
+    print(text, ' gaussian method:\t', compare(y_predicted, y_test))
+    y_predicted = naive_bayes_categorical(train, x_test)
+    print(text, 'categorical method:\t', compare(y_predicted, y_test))
+
+    # print(len(reach))
+    # print(len(y_predicted))
+
+    plt.scatter(range(len(y_predicted)), y_predicted, color=color, marker='x')
+
+
+def execute_naive_bayes(data_frame):
+    discretized_glass_data = discretization(data_frame, 10, 4)
+    normalized_glass_data = normalization(data_frame)
+    standardized_glass_data = standardization(data_frame)
+
+    # print(discretized_glass_data, '\n')
+    # print(normalized_glass_data, '\n')
+    # print(standardized_glass_data, '\n')
+
+    df_concat = pd.concat([data_frame, discretized_glass_data, normalized_glass_data, standardized_glass_data], axis=1)
+    train, test = train_test_split(df_concat, test_size=test_size, random_state=40)
+
+    # Split the train set into individual DataFrames
+    train_data_frame = train.iloc[:, 0:11]
+    train_discretized_glass_data = train.iloc[:, 11:22]
+    train_normalized_glass_data = train.iloc[:, 22:33]
+    train_standardized_glass_data = train.iloc[:, 33:44]
+
+    # Split the test set into individual DataFrames
+    test_data_frame = test.iloc[:, 0:11]
+    x_test_normal = test_data_frame.drop([10], axis=1)
+    y_test_normal = test_data_frame[10]
+    test_discretized_glass_data = test.iloc[:, 11:22]
+    x_test_discretized = test_discretized_glass_data.drop([10], axis=1)
+    y_test_discretized = test_discretized_glass_data[10]
+    test_normalized_glass_data = test.iloc[:, 22:33]
+    x_test_normalized = test_normalized_glass_data.drop([10], axis=1)
+    y_test_normalized = test_normalized_glass_data[10]
+    # test_standardized_glass_data = test.iloc[:, 33:44]
+    # x_test_standardized = test_standardized_glass_data.drop([10], axis=1)
+    # y_test_standardized = test_standardized_glass_data[10]
+
+    # (train, x_test, y_test, text)
+    show_naive_bayes(train_data_frame, x_test_normal, y_test_normal, 'normal data', 'red')
+    show_naive_bayes(train_discretized_glass_data, x_test_discretized, y_test_discretized, 'discretized data', 'magenta')
+    show_naive_bayes(train_normalized_glass_data, x_test_normalized, y_test_normalized, 'normalised', 'blue')
+    # show_naive_bayes(train_standardized_glass_data, x_test_standardized, y_test_standardized, 'standardized', 'cyan')
+
+    plt.title('Custom model regression')
+    plt.xlabel("Test number")
+    plt.ylabel("Number of fish")
+    # plt.scatter(range(len(y_test_normal)), y_test_normal, color='green')
+
+    plt.show()
 
 
 if __name__ == '__main__':
     df = get_data()
+
     # get_stats(df)
+    # get_correlation_heatmap(df)
+
+    df = push_4_to_data(df)
+    # print(df)
 
     temp_glass_type = df.groupby(df[10])
     glass_type = []
     temp_iterator = 0
     for i in temp_glass_type:
         glass_type.append(i[1])
-        temp_iterator += 1
         # get_stats(glass_type[temp_iterator])
         # print('---------------------------------------\n')
+        temp_iterator += 1
 
-    discretized_glass_data = discretization(df, 5, 4)
-    normalized_glass_data = normalization(df)
-    standardized_glass_data = standardization(df)
-
-    # print(discretized_glass_data, '\n')
-    # print(normalized_glass_data, '\n')
-    # print(standardized_glass_data, '\n')
-
-    show_bayes(df, 'normal data')
-    show_bayes(normalized_glass_data, 'normalised')
-    show_bayes(standardized_glass_data, 'standardized')
+    execute_naive_bayes(df)
